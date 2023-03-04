800127077dfd9c52f5232153a05ad353488c7705
梁乐 <lowleung@163.com>
2023-03-04 23:37:30 +0800

    Save

--- meta.js
+++ meta.js
@@ -4,34 +4,17 @@ const fs = require('fs')
 const {
   sortDependencies,
   installDependencies,
-  runLintFix,
   printMessage,
 } = require('./utils')
 const pkg = require('./package.json')
-
-const templateVersion = pkg.version
 
 const { addTestAnswers } = require('./scenarios')
 
 module.exports = {
   metalsmith: {
-    // When running tests for the template, this adds answers for the selected scenario
     before: addTestAnswers
   },
-  // helpers: {
-  //   if_or(v1, v2, options) {
-
-  //     if (v1 || v2) {
-  //       return options.fn(this)
-  //     }
-
-  //     return options.inverse(this)
-  //   },
-  //   template_version() {
-  //     return templateVersion
-  //   },
-  // },
-  
+ 
   prompts: {
     name: {
       when: 'isNotTest',
@@ -80,9 +63,6 @@ module.exports = {
 
     if (data.autoInstall) {
       installDependencies(cwd, data.autoInstall, green)
-        .then(() => {
-          return runLintFix(cwd, data, green)
-        })
         .then(() => {
           printMessage(data, green)
         })
