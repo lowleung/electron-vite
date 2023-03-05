a0cca48b6c73d33d41c3eaf2ce5b078d7085fd8f
梁乐 <lowleung@163.com>
2023-03-05 09:24:31 +0800

    Save

--- scenarios/index.js
+++ scenarios/index.js
@@ -1,19 +1,17 @@
-const scenarios = [
-  'full', 
-  'full-karma-airbnb', 
-  'minimal'
-]
+const index = scenarios.indexOf(process.env.VUE_TEMPL_TEST);
 
-const index = scenarios.indexOf(process.env.VUE_TEMPL_TEST)
+const isTest = (exports.isTest = index !== -1);
 
-const isTest = exports.isTest = index !== -1
-
-const scenario = isTest && require(`./${scenarios[index]}.json`)
+const scenario = {
+  name: "",
+  description: "",
+  autoInstall: false,
+};
 
 exports.addTestAnswers = (metalsmith, options, helpers) => {
   Object.assign(
     metalsmith.metadata(),
     { isNotTest: !isTest },
     isTest ? scenario : {}
-  )
-}
+  );
+};
